56574da305dab77e19f25d1a3055a8ff29f514fa
Dnld <steinert.donald@gmail.com>
2015-09-17 23:36:31 -0400

    adds and retreives links from mongo db

--- app/models/link.js
+++ app/models/link.js
@@ -17,6 +17,14 @@ var linkSchema = new Schema({
 var Link = mongoose.model('Link', linkSchema);
 
 Link.createLink = function(link, res) {
+  // var shasum = crypto.createHash('sha1')
+  // shasum.update(model.url);
+  // link.code = shasum.digest('hex').slice(0, 5);
+  
+  // to do -- fix crypto
+  var temp = Math.floor(Math.random() * 10000);
+  link.code = temp;
+  //console.log(link.code);
   var newLink = new Link(link);
   
   newLink.save(function(err, newLink) {
@@ -26,6 +34,25 @@ Link.createLink = function(link, res) {
     }
     res.send(200, newLink);
   });
+};
+
+Link.fetchUrl = function(req, res, callback) {
+  var result = Link.find({code: req}).select('url');
+  //var result = Link.find.select('code');
+  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', result.code);
+  
+  // Link.findOne({code: req}, 'url', function(err, result, res) {
+  //   if (!result) {
+  //     res.redirect('/');
+  //   } else {
+  //     console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!link found')
+  //     //console.log(result);
+  //     callback(result);
+  //     // link.update({$inc: {visits: 1}}, function(err, result, res) {
+  //     //   callback(link.url);
+  //     // });
+  //   }
+  // });
 };
 
 module.exports = Link;
