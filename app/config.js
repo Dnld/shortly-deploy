6a65b8669bc92ba58565eab9eaaca77b97879c97
Dnld <steinert.donald@gmail.com>
2015-09-17 20:34:10 -0400

    connects to mongodb, passes initial tests

--- app/config.js
+++ app/config.js
@@ -1,45 +1,64 @@
-var Bookshelf = require('bookshelf');
+//var Bookshelf = require('bookshelf');
+var bcrypt = require('bcrypt-nodejs');
 var path = require('path');
 
-var db = Bookshelf.initialize({
-  client: 'sqlite3',
-  connection: {
-    host: '127.0.0.1',
-    user: 'your_database_user',
-    password: 'password',
-    database: 'shortlydb',
-    charset: 'utf8',
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  }
-});
+var mongoose = require('mongoose');
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('base_url', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+mongoose.connect('mongodb://localhost/');
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+mongoose.connection.on('error', console.error.bind(console, 'connection error'));
+mongoose.connection.on('diconnected', mongoose.connect);
 
-module.exports = db;
+module.exports = mongoose;
+
+// db.findUser = function(user, callback) {
+  
+// };
+
+// db.validateUser = function(password, callback) {
+  
+// };
+
+
+// var db = Bookshelf.initialize({
+//   client: 'sqlite3',
+//   connection: {
+//     host: '127.0.0.1',
+//     user: 'your_database_user',
+//     password: 'password',
+//     database: 'shortlydb',
+//     charset: 'utf8',
+//     filename: path.join(__dirname, '../db/shortly.sqlite')
+//   }
+// });
+
+// db.knex.schema.hasTable('urls').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('urls', function (link) {
+//       link.increments('id').primary();
+//       link.string('url', 255);
+//       link.string('base_url', 255);
+//       link.string('code', 100);
+//       link.string('title', 255);
+//       link.integer('visits');
+//       link.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+// db.knex.schema.hasTable('users').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('users', function (user) {
+//       user.increments('id').primary();
+//       user.string('username', 100).unique();
+//       user.string('password', 100);
+//       user.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+
--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,47 @@
 var db = require('../config');
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var linkSchema = new Schema({
+  Id: Schema.Types.ObjectId,
+  url: String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: String,
+  timestamp: { type: Date, default: Date.now }    
 });
 
+var Link = mongoose.model('Link', linkSchema);
+
+Link.createLink = function(link, res) {
+  var newLink = new Link(link);
+  
+  newLink.save(function(err, newLink) {
+    if (err) {
+      console.log('link not added');
+      return;
+    }
+    res.send(200, newLink);
+  });
+};
+
 module.exports = Link;
+
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+//   initialize: function(){
+//     this.on('creating', function(model, attrs, options){
+//       var shasum = crypto.createHash('sha1');
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
+// });
+
